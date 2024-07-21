dcb226ba0431d10a83b64f5759f765b50d526612
Johan-z91 <163840479+Johan-z91@users.noreply.github.com>
2024-07-21 19:20:48 +0000

    FINISH GENERATE DOMAIN JOHAN

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateDomain() {
+  const randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+  const randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  const randomNoun = noun[Math.floor(Math.random() * noun.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const randomDomain = randomPronoun + randomAdj + randomNoun;
+  document.getElementById("domainInput").value = randomDomain;
+}
+
+// Función para generar un dominio basado en una palabra personalizada
+function generateCustomDomain() {
+  const customAdj = document.getElementById("customNounInput").value.trim();
+  if (!customAdj) {
+    alert("Por favor, introduce una palabra para generar el dominio.");
+    return;
+  }
+
+  const randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+  const randomNoun = noun[Math.floor(Math.random() * noun.length)];
+
+  const customDomain = randomPronoun + customAdj + randomNoun;
+  document.getElementById("customDomainInput").value = customDomain;
+}
+
+// Event listeners para los formularios
+document
+  .getElementById("randomDomainForm")
+  .addEventListener("submit", function(event) {
+    event.preventDefault();
+    generateDomain();
+  });
+
+document
+  .getElementById("userNounForm")
+  .addEventListener("submit", function(event) {
+    event.preventDefault();
+    generateCustomDomain();
+  });
